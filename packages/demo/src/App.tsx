a2414aae29f19897b5c839812b97473ff4dcd5d1
ben <x_z_gfree@163.com>
2024-04-16 09:59:48 +0800

    feature: add init for some feature.

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Show, For } from "react-helm";
+import Show from "react-helm/lib/Show";
+import For from "react-helm/lib/For";
 
 function App() {
   const [count, setCount] = useState(0);
